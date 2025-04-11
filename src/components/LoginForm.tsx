a79449d3aa9467398274c934f47a22b5d2b4b23a
Gonzalo Fontes <goonzuuy@192.168.1.7>
2025-04-10 21:12:00 -0300

    basic login form implementation with new styles

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import "../styles/styles.css";
+
 const LoginForm = () => {
-    return <>'Refactor'</>
-}
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form className="login_form" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+};
 
 export default LoginForm;
